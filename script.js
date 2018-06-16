9693eea3cf64be60326c2e1b768eaa10380f047d
Nader T G <ng1312@ic.ac.uk>
2018-06-16 13:20:45 -0400

    updated script.js

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
+
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
+var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
 //recognition.continuous = false;
 recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
 var diagnostic = document.querySelector('.output');
 var bg = document.querySelector('html');
 var hints = document.querySelector('.hints');
 
-hints.innerHTML = 'Tap/click then say something';
+var colorHTML= '';
+
+hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try.';
 
 document.body.onclick = function() {
   recognition.start();
-  console.log('Ready to transcribe. ');
+  console.log('Ready to receive a speech');
 }
 
 recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The [last] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
 
+  var last = event.results.length - 1;
+  var color = event.results[last][0].transcript;
   var spokenText = event.results[0][0].transcript;
-
-  if (event.results[0].isFinal) {
-    hints.innerHTML = speechToText;
-  }
+  diagnostic.textContent = 'Result received: ' +  spokenText + '.';
+  
+  console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
 recognition.onspeechend = function() {
@@ -32,7 +51,7 @@ recognition.onspeechend = function() {
 }
 
 recognition.onnomatch = function(event) {
-  diagnostic.textContent = "I didn't recognise that word.";
+  diagnostic.textContent = "I didn't recognise that color.";
 }
 
 recognition.onerror = function(event) {
