fc485f2416fcf0a831a374e0384629d011634206
Nader T G <ng1312@ic.ac.uk>
2018-06-16 13:37:00 -0400

    updated script.js

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
-
 var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-//recognition.continuous = false;
+
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
@@ -43,8 +37,8 @@ recognition.onresult = function(event) {
   var last = event.results.length - 1;
   var color = event.results[last][0].transcript;
   var speach = event.results[0][0].transcript;
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  diagnostic.textContent = 'Result received2 : ' + speach + '.';
+ // diagnostic.textContent = 'Result received: ' + color + '.';
+  diagnostic.textContent = 'Result received : ' + speach + '.';
   //bg.style.backgroundColor = color;
   console.log('Confidence: ' + event.results[last][0].confidence);
 }
