2153f52a9d177a706a673cc79a7689b80e7ec549
Nader T G <ng1312@ic.ac.uk>
2018-06-16 13:25:04 -0400

    updated script.js

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ colors.forEach(function(v, i, a){
   console.log(v, i);
   colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
 });
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
+hints.innerHTML = 'Tap/click then speak';
 
 document.body.onclick = function() {
   recognition.start();
-  console.log('Ready to receive a color command.');
+  console.log('Listening');
 }
 
 recognition.onresult = function(event) {
@@ -42,10 +42,11 @@ recognition.onresult = function(event) {
 
   var last = event.results.length - 1;
   var color = event.results[last][0].transcript;
-
+  var speach = event.results[0][0].transcript;
   diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
-  console.log('Confidence: ' + event.results[0][0].confidence);
+  diagnostic.textContent = 'Result received2 : ' + speach + '.';
+  //bg.style.backgroundColor = color;
+  console.log('Confidence: ' + event.results[last][0].confidence);
 }
 
 recognition.onspeechend = function() {
